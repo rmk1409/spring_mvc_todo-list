cf38c8257d48a2b7918735a59216cb10a3634e96
Roman Pogorelov <rmk140987@gmail.com>
2019-12-25 07:54:22 +0300

    Add dispatcher servlet

---
+++ src/main/java/com/veselov/alex/config/WellAppInitializer.java
@@ -0,0 +1,26 @@
+package com.veselov.alex.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+@Slf4j
+public class WellAppInitializer implements WebApplicationInitializer {
+    private static final String DISPATCHER_SERVLET_NAME = "dispatcher";
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        log.info("onStartup");
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.register(WebConfig.class);
+        DispatcherServlet dispatcher = new DispatcherServlet(context);
+        ServletRegistration.Dynamic registration = servletContext.addServlet(DISPATCHER_SERVLET_NAME, dispatcher);
+        registration.setLoadOnStartup(1);
+        registration.addMapping("/");
+    }
+}
