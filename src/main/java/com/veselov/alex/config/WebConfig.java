c39ff712c2d96b741018471dc40a5eb02e5c2da0
Pogorelov Roman <rmk140987@gmail.com>
2019-12-25 10:07:25 +0300

    Add ViewResolver and simple Controller

--- src/main/java/com/veselov/alex/config/WebConfig.java
+++ src/main/java/com/veselov/alex/config/WebConfig.java
@@ -1,12 +1,25 @@
 package com.veselov.alex.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 @EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = "com.veselov.alex")
 public class WebConfig {
-    
+    public static final String RESOLVER_PREFIX = "/WEB-INF/view/jsp/";
+    public static final String RESOLVER_SUFFIX = ".jsp";
+
+    @Bean
+    public ViewResolver viewResolver() {
+        UrlBasedViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix(RESOLVER_PREFIX);
+        resolver.setSuffix(RESOLVER_SUFFIX);
+        return resolver;
+    }
 }
---
+++ src/main/java/com/veselov/alex/controller/DemoController.java
@@ -0,0 +1,21 @@
+package com.veselov.alex.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DemoController {
+
+    @ResponseBody
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("welcome")
+    public String welcome() {
+        return "welcome";
+    }
+
+}
