7c058ec867b4983d52ff9cfe610c44b144c3bc8c
Pogorelov Roman <rmk140987@gmail.com>
2019-12-25 12:11:43 +0300

    Add Util classes

---
+++ src/main/java/com/veselov/alex/util/Mappings.java
@@ -0,0 +1,6 @@
+package com.veselov.alex.util;
+
+public final class Mappings {
+    public static final String ITEMS = "items";
+    private Mappings() {}
+}
---
+++ src/main/java/com/veselov/alex/util/ViewNames.java
@@ -0,0 +1,8 @@
+package com.veselov.alex.util;
+
+public final class ViewNames {
+    public static final String ITEMS_LIST = "items_list";
+
+    private ViewNames() {
+    }
+}
